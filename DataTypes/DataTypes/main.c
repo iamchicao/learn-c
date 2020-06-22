308dbcf1facb5141f481eb584a9a0cb288355a38
Francisco Junior <jr-francis2011@hotmail.com>
2020-06-22 20:38:44 -0300

    Data Types

---
+++ DataTypes/DataTypes/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Criando uma variável e atribuindo um valor a ela
+    int age = 45;
+    //Criando uma variável de valor flutuante
+    double gpa = 4.5;
+    //Criando uma variável e atribuindo uma string a ela
+    char grade = 'A';
+    char phrase[] = "Internet School";
+
+    return 0;
+}
